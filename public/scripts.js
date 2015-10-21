a7233a864476b64733779899edfde9a511dde80a
Jens <soppafisk@gmail.com>
2015-10-21 14:19:36 +0200

    chat

---
+++ public/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  var host = "ws://localhost:3000";
+  var ws = new WebSocket(host);
+
+  var feedbackDiv = $('#feedback');
+  // ws.send('hej//klienten');
+  var name;
+  ws.onopen = function() {
+    name = prompt("vad heter du?");
+  }
+
+  ws.onmessage = function(event) {
+    // console.log(event.data);
+    var data = $.parseJSON(event.data);
+    var name = data.name;
+    feedbackDiv.prepend(data.name + ": " + data.message + "<br>");
+  };
+
+  $('#send-message').click(function() {
+    if (ws.readyState != WebSocket.OPEN) throw new Error('Not connected');
+    var message = $('#the-message').val();
+    var package = {message: message, name: name};
+    ws.send(JSON.stringify(package));
+    $('#the-message').val("");
+  });
+});
---
+++ server.js
@@ -0,0 +1,31 @@
+var server = require('http').createServer();
+var url = require('url');
+var express = require('express');
+var fs = require('fs');
+var WebSocketService = require('ws').Server;
+var wss = new WebSocketService({server: server});
+
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+wss.on('connection', function(ws){
+  ws.on('message', function(message) {
+    //console.log('received: %s', message);
+    wss.broadcast(message);
+  });
+  //ws.send('Välkommen!');
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data, function(error) {
+      console.log(error);
+    });
+  });
+};
+
+server.on('request', app);
+server.listen('3000', function () {
+  console.log('Listening on ' + server.address().port);
+});
